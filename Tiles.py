62a924ff2f805a8668f70210b75613308fb02665
Leon Eckert <eckertleon@gmail.com>
2016-07-28 00:51:13 +0200

    almost done with imgur and yandex

--- Tiles.py
+++ Tiles.py
@@ -1,4 +1,7 @@
 import PIL
+import os
+from bs4 import BeautifulSoup
+import urllib2
 
 class Tile:
     """docstring for Tile"""
@@ -8,10 +11,48 @@ class Tile:
         self.yidx = yidx
         self.idx = idx
         self.name = "tile_" + str(idx)
+        
+        self.path = None
+        self.upload_data = None
+        self.imgur_id = None
+        self.link = None
 
     def show(self):
         self.im.show()
-        
+
+    def save_temp(self, path="data/temp"):
+        self.path = os.path.join(path, self.name+".jpg")
+        self.im.save(self.path)
+        print "[+] Saved", self.name, "at", self.path
+
+    def remove_temp(self, path="data/temp"):
+        if self.path is not None:
+            os.remove(self.path)
+            print "[+] Removed", self.name, "from", self.path
+        else:
+            print "[-] tried to remove", self.name, "but couldn't find a path."
+    
+    def upload_imgur(self, client): 
+        # tile.upload_imgur(client)
+        try:
+            self.upload_data = client.upload_from_path(self.path, config=None, anon=True)
+            self.imgur_id = self.upload_data["id"]
+            self.link = self.upload_data["link"]
+            # {u'datetime': 1469655152, u'bandwidth': 0, u'nsfw': None, u'vote': None, u'id': u'W1KkfXA', u'account_id': 0, u'in_gallery': False, u'title': None, u'section': None, u'width': 960, u'size': 12100, u'type': u'image/jpeg', u'is_ad': False, u'deletehash': u'RXuInXyOGiS98wj', u'description': None, u'views': 0, u'link': u'http://i.imgur.com/W1KkfXA.jpg', u'height': 144, u'name': u'', u'favorite': False, u'account_url': None, u'animated': False}
+            print "[+] UPLOADED", self.name, "TO imgur with link", self.link, "and id", self.imgur_id
+        except ImgurClientError as e:
+            print(e.error_message)
+            print(e.status_code)
+
+
+
+    # def delete_imgur(self, client):
+    #     try:
+    #         print client.delete_image(self.imgur_id)
+    #         print "[+] DELETED", self.name, "FROM imgur with link", self.link, "and id", self.imgur_id
+    #     except:
+    #         pass
+    #         print "[-] problems deleting", self.name, "FROM imgur with link", self.link, "and id", self.imgur_id
   
 class Tiles(object):
     """docstring for Tiles"""
@@ -30,12 +71,9 @@ class Tiles(object):
         idx = 0
         for y in range(self.rows):
             for x in range(self.columns):
-
                 y_pos = (y*self.tile_h)
-                x_pos = (x*self.tile_w)
-                             
+                x_pos = (x*self.tile_w)                  
                 tile = self.image.crop(( x_pos,y_pos, x_pos+self.tile_w,   y_pos+self.tile_h  ))
-
                 tiles_out.append( Tile( tile, x, y, idx))
                 idx += 1
         return tiles_out
@@ -43,3 +81,50 @@ class Tiles(object):
     def open_all(self):
         for tile in self.tiles:
             tile.show()
+
+    def save_temp(self, path="data/temp"):
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        for tile in self.tiles:
+            tile.save_temp(path="data/temp")
+
+    def remove_temp(self, path="data/temp"):
+        for tile in self.tiles:
+            tile.remove_temp(path="data/temp")
+
+    def upload_imgur(self, client):
+        for tile in self.tiles:
+            tile.upload_imgur(client)
+
+    # def delete_imgur(self, client):
+    #     for tile in self.tiles:
+    #         tile.delete_imgur(client)
+
+    def process(self):
+        base_url = 'https://yandex.ru/images/search?rpt=imageview&img_url='
+        for tile in self.tiles:
+            url = base_url + tile.link
+            html = urllib2.urlopen(url).read()
+            soup = BeautifulSoup(html, 'html.parser')
+            
+            imgs = soup.find_all('img')
+            similar_imgs = list()
+            for img in imgs:
+                try:
+                    if 'similar__image' in img['class']:
+                        similar_img = "https:"+str(img['src'])
+                        similar_imgs.append(similar_img)
+                except KeyError:
+                    print "no class here"
+                    pass
+            print similar_imgs
+            break
+
+
+
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from imgurpython import ImgurClient
 from PIL import Image
 import Tiles as t
 import requests
+import ConfigParser
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -12,8 +13,17 @@ ap.add_argument("-c", "--columns", default=2, type=int, help="Path to the image"
 ap.add_argument("-r", "--rows", default=2, type=int, help="Path to the image")
 args = vars(ap.parse_args())
 
-if __name__ == '__main__':
+config = ConfigParser.ConfigParser()
+try:
+    config.read('settings.cfg')
+    print "[+] Read settings"
+except:
+    print "[-] Could not read settings"
+client_id = config.get('imgur','client_id')
+client_secret = config.get('imgur','client_secret')
 
+
+if __name__ == '__main__':
 
     # image = cv2.imread(args["image"])
     image = Image.open(args["image"])
@@ -24,13 +34,44 @@ if __name__ == '__main__':
 
     tiles = t.Tiles(image, columns, rows)
     
-    tiles.open_all()
-    cv2.waitKey(0)
+    # tiles.open_all()
+    # cv2.waitKey(0)
+
+    
 
 
 
-get_image(image_id)
-upload_from_path(path, config=None, anon=True)
-upload_from_url(url, config=None, anon=True)
-delete_image(image_id)
-favorite_image(image_id)
+    
+    client = ImgurClient(client_id, client_secret)
+
+    tiles.save_temp()
+    tiles.upload_imgur(client)
+    
+
+    tiles.process()
+
+
+    # tiles.delete_imgur(client)
+
+
+    tiles.remove_temp()
+
+
+
+
+# client_id = 'YOUR CLIENT ID'
+# client_secret = 'YOUR CLIENT SECRET'
+
+# client = ImgurClient(client_id, client_secret)
+
+# get_image(image_id)
+# upload_from_path(path, config=None, anon=True)
+# upload_from_url(url, config=None, anon=True)
+# delete_image(image_id)
+# favorite_image(image_id)
+
+
+
+    # configDomain = config.get('google','domain')
+    # configEmail = config.get('google','email')
+    # configPass = config.get('google','psswrd')
